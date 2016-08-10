261a29f5fa0a85333568a8e36655b09a5b425f30
Daniel Pereira <zdaniel.gp@gmail.com>
2016-08-10 10:44:03 -0300

    Fix gnupg plugin

--- plugins/gnupg/gnupg.plugin.zsh
+++ plugins/gnupg/gnupg.plugin.zsh
@@ -11,4 +11,4 @@ alias gpg='gpg2'
 # This commands are meant to encrypt without metadata such as the key of the recipients.
 # This way a message is "meaningless" if the recipient doesn't know to whom it was encrypted
 alias gpg-anonymous-encrypt='gpg2 --encrypt --no-emit-version --no-comments --throw-keyids --armor'
-alias gpg-anonymous-decrypt='gpg2 --decrypt --default-recipient-self'
+alias gpg-anonymous-decrypt='gpg2 --decrypt --try-secret-key $GPG_MAIN_KEY'
