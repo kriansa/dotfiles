bd8461ce30580a63f2c802c9786ca1d6f2f91e06
Daniel Pereira <zdaniel.gp@gmail.com>
2016-12-04 12:27:48 -0200

    Update config.zsh

--- config.zsh
+++ config.zsh
@@ -75,12 +75,5 @@ plugins=(
 # You may need to manually set your language environment
 # export LANG=en_US.UTF-8
 
-# Preferred editor for local and remote sessions
-# if [[ -n $SSH_CONNECTION ]]; then
-#   export EDITOR='vim'
-# else
-#   export EDITOR='mvim'
-# fi
-
 # Compilation flags
 # export ARCHFLAGS="-arch x86_64"
