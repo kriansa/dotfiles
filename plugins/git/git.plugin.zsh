59352e8b1e071088e9d6dcd27a3139ceb10d948e
Daniel Pereira <zdaniel.gp@gmail.com>
2017-03-10 12:56:12 -0300

    Add 'g' alias for 'git'

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -10,5 +10,6 @@ zstyle -s ":vcs_info:git:*:-all-" "command" _omz_git_git_cmd
 : ${_omz_git_git_cmd:=git}
 
 # 2. Useful aliases
+alias g='git'
 alias gwip='git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit -m "--wip--"'
 alias gunwip='git log -n 1 | grep -q -c "\-\-wip\-\-" && git reset HEAD~1'
