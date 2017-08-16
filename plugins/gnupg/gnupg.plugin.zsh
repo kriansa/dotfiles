e6cf888e20909fd11d323dce5b15f90b49076551
Daniel Pereira <zdaniel.gp@gmail.com>
2017-08-16 10:25:39 -0300

    Remove alias to gpg2
    It has been removed on GnuPG 2.1.21

--- plugins/gnupg/gnupg.plugin.zsh
+++ plugins/gnupg/gnupg.plugin.zsh
@@ -6,14 +6,11 @@ PLUGIN_PATH=$0:A:h
 export PATH="$PLUGIN_PATH/bin:$PATH"
 export SSH_AUTH_SOCK=$HOME/.gnupg/S.gpg-agent.ssh
 
-# 2. Alias gpg to gpg2
-alias gpg='gpg2'
-
 # These commands are meant to encrypt without metadata such as the key of the
 # recipients. This way a message is "meaningless" if the recipient doesn't
 # know to whom it was encrypted
-alias gpg-anonymous-encrypt='gpg2 --encrypt --no-emit-version --no-comments --throw-keyids --armor'
-alias gpg-anonymous-decrypt='gpg2 --decrypt --try-secret-key $GPG_MAIN_KEY'
+alias gpg-anonymous-encrypt='gpg --encrypt --no-emit-version --no-comments --throw-keyids --armor'
+alias gpg-anonymous-decrypt='gpg --decrypt --try-secret-key $GPG_MAIN_KEY'
 
 # Sometimes GPG can be a PITA when you remove the card. When that happen, we
 # just need to restart it
