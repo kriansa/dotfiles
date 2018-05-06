87ba5fff2e96107368fc9a8eeeca77e46ce8616f
Daniel Pereira <zdaniel.gp@gmail.com>
2018-05-06 11:52:32 -0300

    Fix gnupg plugin

--- plugins/gnupg/gnupg.plugin.zsh
+++ plugins/gnupg/gnupg.plugin.zsh
@@ -4,7 +4,12 @@ PLUGIN_PATH=$0:A:h
 
 # 1. Exports
 export PATH="$PLUGIN_PATH/bin:$PATH"
-export SSH_AUTH_SOCK=$HOME/.gnupg/S.gpg-agent.ssh
+# Export variables as recommended by 'man gpg-agent'
+export GPG_TTY=$(tty)
+unset SSH_AGENT_PID
+if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
+  export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
+fi
 
 # These commands are meant to encrypt without metadata such as the key of the
 # recipients. This way a message is "meaningless" if the recipient doesn't
