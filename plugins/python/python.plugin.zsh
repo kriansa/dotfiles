55015c9af10f312fd45fb25ffa5cbb59f3c9812e
Daniel Pereira <zdaniel.gp@gmail.com>
2018-12-28 16:23:18 -0200

    python: on MacOS, export the path to packages to $PATH

--- plugins/python/python.plugin.zsh
+++ plugins/python/python.plugin.zsh
@@ -1,5 +1,10 @@
 # 1. Exports
-# Add brew bin path to Python 3 when on Mac
 if [[ "$OSTYPE" == darwin* ]]; then
+  # Add brew bin path to Python 3 when on Mac
   export PATH="/usr/local/opt/python/libexec/bin:$PATH"
+
+  # Add the user package bin path
+  # This path is obtainable through the following command:
+  # $ python -m site --user-base
+  export PATH="$HOME/Library/Python/3.7/bin:$PATH"
 fi
