913634d62dc7b3a6e3eff5995be73037536b964a
Daniel Pereira <zdaniel.gp@gmail.com>
2019-04-02 18:20:58 -0300

    fortunes: move my sayings out of dotfiles

--- plugins/fortunes/fortunes.plugin.zsh
+++ plugins/fortunes/fortunes.plugin.zsh
@@ -1,8 +1,5 @@
-# Fortunes plugin
-PLUGIN_PATH=$0:A:h
-
 # 1. Run cowsay on every new terminal spawn
-[[ $- = *i* ]] && cat $PLUGIN_PATH/data/*.txt | sort --random-sort | head -1 | cowsay
+[[ $- = *i* ]] && cat $HOME/Dropbox/Fortunes/*.txt | sort --random-sort | head -1 | cowsay
 
 # 2. Disable cowsay for ansible
 export ANSIBLE_NOCOWS=1
