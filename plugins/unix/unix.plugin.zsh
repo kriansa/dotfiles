21a5bdc4e8d4a394e6a3ae898fc6f6f0f2aabd7c
Daniel Pereira <zdaniel.gp@gmail.com>
2019-06-17 02:30:28 -0300

    unix: create new iftun & tunssh helpers

--- plugins/unix/unix.plugin.zsh
+++ plugins/unix/unix.plugin.zsh
@@ -3,3 +3,11 @@ PLUGIN_PATH=$0:A:h
 
 # 1. Exports
 export PATH="$PLUGIN_PATH/bin:$PATH"
+
+# 2. Function to help SSH'ing using tun0
+function tunssh {
+  iftun tun0 SSH_AUTH_SOCK="$SSH_AUTH_SOCK" ssh "$@"
+}
+
+# Add this plugin path to the fpath completion list
+fpath+=($PLUGIN_PATH/completions)
