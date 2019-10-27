03e7b90d79fc7427da0f9aa9da58137d816bc075
Daniel Pereira <zdaniel.gp@gmail.com>
2019-10-27 11:30:30 -0300

    chore(gnupg): move pinentry-gui from lastpass to gnupg namespace

--- plugins/lastpass/lastpass.plugin.zsh
+++ plugins/lastpass/lastpass.plugin.zsh
@@ -1,6 +1,2 @@
-# Plugin path
-PLUGIN_PATH=$0:A:h
-
 # 1. Exports
-export PATH="$PLUGIN_PATH/bin:$PATH"
-export LPASS_PINENTRY=$PLUGIN_PATH/bin/pinentry-gui
+export LPASS_PINENTRY="$DOTFILES_PATH/plugins/gnupg/bin/pinentry-gui"
