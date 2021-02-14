49006c7385b9fb8f35531139e3d94bd503545c2f
Daniel Pereira <zdaniel.gp@gmail.com>
2021-02-14 20:55:06 -0300

    project-boilerplates: update terraform aws setup

--- data/project-boilerplates/aws-cloud/ops/terraform/aws.tf
+++ data/project-boilerplates/aws-cloud/ops/terraform/aws.tf
@@ -1,8 +1,15 @@
 # Declare an AWS provider
 # =======================
-provider "aws" {
-  version = "~> 2.0"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
 }
+
+provider "aws" {}
 
 # Common AWS data
 # ===============
