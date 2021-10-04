4ec8d03ff7f43148cb97e4844f57d6fafd164a64
Daniel Pereira <zdaniel.gp@gmail.com>
2021-10-04 18:23:21 -0300

    devops: remove shell aliases as they are now bins as of 02e9877

--- plugins/devops/devops.plugin.zsh
+++ plugins/devops/devops.plugin.zsh
@@ -9,21 +9,6 @@ alias d='docker'
 alias dc='docker-compose'
 alias k='kubectl'
 
-k-list-all() {
-	namespace=$1
-
-	kubectl api-resources --verbs=list --namespaced -o name | \
-		xargs -n 1 kubectl get --show-kind --ignore-not-found -n $namespace
-}
-
-# This helps calling a URL by resolving its address to any arbitrary IP
-debug-tls-cert() {
-  ip=$1
-  host=$2
-
-  curl -kvv --no-keepalive -H "Host: $host" --resolve "$host:443:$ip" "https://$host"
-}
-
 # 3. Enable autocompletion commands
 # Enable autocompletion for terraform
 if [ $commands[terraform] ]; then
