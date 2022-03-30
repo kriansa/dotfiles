708f8b551c8c27cc8adc151a8b992ea3b685c9c4
Daniel Pereira <daniel@garajau.com.br>
2022-03-30 13:49:10 -0300

    nvim: adapt paste-no-yank to work with both p and P

--- modules/nvim/include/.config/nvim/lua/custom_plugins/paste-no-yank.lua
+++ modules/nvim/include/.config/nvim/lua/custom_plugins/paste-no-yank.lua
@@ -9,18 +9,20 @@ function M.restore_registers()
   vim.fn.setreg("\"", M.registers.b)
 end
 
-function M.paste_over()
+function M.paste_over(cmd)
   M.registers.a = vim.fn.getreg("+")
   M.registers.b = vim.fn.getreg("\"")
-  return "p:lua require('custom_plugins.paste-no-yank').restore_registers()\n"
+  return cmd .. ":lua require('custom_plugins.paste-no-yank').restore_registers()\n"
 end
 
 function M.setup()
   vim.cmd([[
-    function! PasteOver()
-      return luaeval("require('custom_plugins.paste-no-yank').paste_over()")
+    function! PasteOver(cmd)
+      return luaeval("require('custom_plugins.paste-no-yank').paste_over('" . a:cmd . "')")
     endfunction
-    xnoremap <silent> <expr> p PasteOver()
+
+    xnoremap <silent> <expr> p PasteOver("p")
+    xnoremap <silent> <expr> P PasteOver("P")
   ]])
 end
 
