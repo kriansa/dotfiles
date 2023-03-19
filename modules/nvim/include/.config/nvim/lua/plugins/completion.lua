b8da36324fff841cf28b60e2f9809d2e213bc69a
Daniel Pereira <daniel@garajau.com.br>
2023-03-18 21:03:47 -0300

    nvim: update settings

--- modules/nvim/include/.config/nvim/lua/plugins/completion.lua
+++ modules/nvim/include/.config/nvim/lua/plugins/completion.lua
@@ -1,15 +1,25 @@
 return function(use)
   use {
     'hrsh7th/nvim-cmp',
-    requires = { 'hrsh7th/cmp-nvim-lsp', 'hrsh7th/cmp-buffer', 'hrsh7th/cmp-path' },
+    requires = { 'hrsh7th/cmp-nvim-lsp', 'hrsh7th/cmp-buffer', 'hrsh7th/cmp-path', 'hrsh7th/cmp-cmdline' },
     config = function()
       local cmp = require('cmp')
+
       cmp.setup({
-        sources = {
+        mapping = cmp.mapping.preset.insert(),
+        sources = cmp.config.sources({
           { name = 'nvim_lsp' },
           { name = 'buffer' },
           { name = 'path' },
-        },
+        })
+      })
+
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = 'path' },
+          { name = 'cmdline' },
+        })
       })
     end,
   }
