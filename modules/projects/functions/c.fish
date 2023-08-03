4c9e416a85ad8eac954df079dbe9bce34bfdfc78
Daniel Pereira <daniel@garajau.com.br>
2023-08-03 04:02:12 -0300

    nvim: update settings

--- modules/projects/functions/c.fish
+++ modules/projects/functions/c.fish
@@ -1,4 +1,4 @@
 function c --description="Quickly jumps into a project's directory"
-  tmux rename-window (string trim --chars=/ $argv[1])
+  test -n "$argv[1]"; and tmux rename-window (string trim --chars=/ $argv[1])
   cd "$PROJECTS/$argv[1]"
 end
