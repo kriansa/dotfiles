b348e3ff53466161b5ffb964a6e868d9d59e873d
Daniel Pereira <daniel@garajau.com.br>
2023-08-15 17:54:10 -0300

    chore: update dote.fish

--- modules/meta/functions/dote.fish
+++ modules/meta/functions/dote.fish
@@ -1,4 +1,6 @@
 function dote --description="Edit dotfiles"
+  set prev_name (tmux display-message -p '#W')
   tmux rename-window dotfiles
   $EDITOR "$DOTFILES_PATH"
+  tmux rename-window "$prev_name"
 end
