51bed58e0fe2b67ad071fae3a6caaa6ee67d8302
Daniel Pereira <daniel@garajau.com.br>
2024-06-21 18:15:55 -0300

    tmux: update settings

--- modules/projects/functions/c.fish
+++ modules/projects/functions/c.fish
@@ -1,4 +1,7 @@
 function c --description="Quickly jumps into a project's directory"
-  test -n "$argv[1]"; and tmux rename-window (string trim --chars=/ $argv[1])
   cd "$PROJECTS/$argv[1]"
+
+  if test $status -eq 0; and test -n "$argv[1]"
+    tmux rename-window (string trim --chars=/ $argv[1])
+  end
 end
