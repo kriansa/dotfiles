90ea82223db55ed71185ae98b195c1f800a7ddbf
Daniel Pereira <daniel@garajau.com.br>
2024-09-26 06:03:24 -0300

    term: update settings

--- modules/fish/include/.config/fish/config.fish
+++ modules/fish/include/.config/fish/config.fish
@@ -10,7 +10,13 @@ if status is-interactive
   # layout=reverse lists results top to bottom, mimicking the familiar layouts of git log, history, and env
   # preview-window=wrap wraps long lines in the preview window, making reading easier
   # marker=+ makes the multi-select marker more distinguishable from the pointer (since both default to >)
-  set --global --export FZF_DEFAULT_OPTS "--bind=ctrl-d:half-page-down,ctrl-u:half-page-up,ctrl-b:page-up,ctrl-f:page-down --cycle --layout=reverse --preview-window=wrap --marker='+'"
+  set --global --export FZF_DEFAULT_OPTS "\
+    --color=bg+:#ccd0da,bg:#eff1f5,spinner:#dc8a78,hl:#d20f39 \
+    --color=fg:#4c4f69,header:#d20f39,info:#8839ef,pointer:#dc8a78 \
+    --color=marker:#7287fd,fg+:#4c4f69,prompt:#8839ef,hl+:#d20f39 \
+    --color=selected-bg:#bcc0cc \
+    --bind=ctrl-d:half-page-down,ctrl-u:half-page-up,ctrl-b:page-up,ctrl-f:page-down \
+    --cycle --multi --layout=reverse --preview-window=wrap --marker='+'"
 
   #  Configure fzf.fish
   set --global fzf_fd_opts --hidden --exclude=.git
