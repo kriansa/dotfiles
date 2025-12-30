477f94c323d7b0ab8413299c929557f1e6111cdc
Daniel Pereira <daniel@garajau.com.br>
2025-12-30 01:09:10 -0300

    fish: update to 4.3

--- modules/shell/init.fish
+++ modules/shell/init.fish
@@ -31,6 +31,35 @@ set --global fish_prompt_pwd_dir_length 10
 # Custom color
 set --global hydro_color_jobs cyan
 
+# Set theme based on fish 3.0
+set --global fish_color_autosuggestion 555 brblack
+set --global fish_color_cancel -r
+set --global fish_color_command 005fd7
+set --global fish_color_comment 990000
+set --global fish_color_cwd green
+set --global fish_color_cwd_root red
+set --global fish_color_end 009900
+set --global fish_color_error ff0000
+set --global fish_color_escape 00a6b2
+set --global fish_color_history_current --bold
+set --global fish_color_host normal
+set --global fish_color_host_remote yellow
+set --global fish_color_normal normal
+set --global fish_color_operator 00a6b2
+set --global fish_color_param 00afff
+set --global fish_color_quote 999900
+set --global fish_color_redirection 00afff
+set --global fish_color_search_match white --background=brblack
+set --global fish_color_selection white --bold --background=brblack
+set --global fish_color_status red
+set --global fish_color_user brgreen
+set --global fish_color_valid_path --underline
+set --global fish_pager_color_completion
+set --global fish_pager_color_description B3A06D yellow
+set --global fish_pager_color_prefix normal --bold --underline
+set --global fish_pager_color_progress brwhite --background=cyan
+set --global fish_pager_color_selected_background -r
+
 # Add navigational helpers
 # (these functions couldn't be in their dedicated files because they would have weird names)
 function ...
