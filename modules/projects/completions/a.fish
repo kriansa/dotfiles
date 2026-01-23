6372c73a34a1570dbaefb4aa740e94ad485f7314
Daniel Pereira <daniel@garajau.com.br>
2026-01-22 22:02:50 -0300

    projects: remove trailing bar from completion

--- modules/projects/completions/a.fish
+++ modules/projects/completions/a.fish
@@ -1,3 +1,3 @@
 complete --command a --no-files --description="Project dir" \
-  --arguments="(complete --do-complete=\"'' $PROJECTS/\" | string replace '$PROJECTS/' '')" \
+  --arguments="(complete --do-complete=\"'' $PROJECTS/\" | string replace '$PROJECTS/' '' | string replace --regex '/\$' '')" \
   --condition="__fish_is_first_token"
--- modules/projects/completions/c.fish
+++ modules/projects/completions/c.fish
@@ -1,3 +1,3 @@
 complete --command c --no-files --description="Project dir" \
-  --arguments="(complete --do-complete=\"'' $PROJECTS/\" | string replace '$PROJECTS/' '')" \
+  --arguments="(complete --do-complete=\"'' $PROJECTS/\" | string replace '$PROJECTS/' '' | string replace --regex '/\$' '')" \
   --condition="__fish_is_first_token"
