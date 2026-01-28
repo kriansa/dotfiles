04a46f96a8155314c07933d2946f22eb02d86caf
Daniel Pereira <daniel@garajau.com.br>
2026-01-28 05:29:19 -0300

    ai: remove need for claude wrapper

--- modules/ai/init.fish
+++ modules/ai/init.fish
@@ -2,5 +2,14 @@
 # See: https://github.com/anthropics/claude-code/issues/1913
 set --global --export ENABLE_INCREMENTAL_TUI true
 
+# Disable auto updater, /bug command, error reporting and telemetry
+set --global --export CLAUDE_CODE_DISABLE_NONESSENTIAL_TRAFFIC 1
+
+# Remove need for janky wrapper on .local/bin
+set --global --export DISABLE_INSTALLATION_CHECKS 1
+
+# Enable tool search always
+set --global --export ENABLE_TOOL_SEARCH true
+
 # Set which code agent we're using
 set --global --export CODE_AGENT claude
