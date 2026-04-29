f7bd63a0e3468602158b8fc78f89206c892b2403
Daniel Pereira <daniel@garajau.com.br>
2026-04-28 22:25:37 -0300

    ghostty: update keybinding config

--- modules/fish/include/.config/fish/functions/fish_user_key_bindings.fish
+++ modules/fish/include/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,8 +1,8 @@
 # Toggle the sudo suffix in the CLI with Ctrl-S (this is already built-in on Fish through Alt+S)
-bind \cs "fish_commandline_prepend sudo"
+bind ctrl-s "fish_commandline_prepend sudo"
 
 # Opens current path with $EDITOR
-bind \ce "$EDITOR ."
+bind ctrl-shift-e "$EDITOR ."
 
 # Edit the current line in $EDITOR (C-X, C-E)
-bind \cx\ce edit_command_buffer
+bind ctrl-x,ctrl-e edit_command_buffer
