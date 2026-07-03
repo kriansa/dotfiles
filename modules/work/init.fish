65dc0329ad196e76a3c0b8b8caf77a975ca7f5e9
Daniel Pereira <daniel@garajau.com.br>
2026-07-03 14:01:36 -0300

    work: update settings to work with homebrew

--- modules/work/init.fish
+++ modules/work/init.fish
@@ -3,3 +3,6 @@ source $DOTFILES_PATH/modules/work/private.fish
 
 # Useful aliases
 alias pubkey="cat ~/.ssh/id_ed25519.pub | pbcopy && echo Key copied!"
+
+# Ensure that brew uses the same SSH command
+set --global --export GIT_SSH_COMMAND "ssh -o IdentitiesOnly=yes -i ~/.ssh/id_ed25519"
