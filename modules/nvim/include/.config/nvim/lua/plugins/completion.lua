7cf29df731101f080f07e5b72b99897f9dc33810
Daniel Pereira <daniel@garajau.com.br>
2026-07-08 20:50:24 -0300

    nvim: remove friendly-snippets

--- modules/nvim/include/.config/nvim/lua/plugins/completion.lua
+++ modules/nvim/include/.config/nvim/lua/plugins/completion.lua
@@ -1,9 +1,6 @@
 return {
   {
     'saghen/blink.cmp',
-    dependencies = {
-      'rafamadriz/friendly-snippets',
-    },
     version = '1.*',
 
     opts = {
@@ -48,7 +45,7 @@ return {
       -- Default list of enabled providers defined so that you can extend it
       -- elsewhere in your config, without redefining it, due to `opts_extend`
       sources = {
-        default = { 'lsp', 'path', 'snippets', 'buffer' },
+        default = { 'lsp', 'path', 'buffer' },
       },
 
       -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
